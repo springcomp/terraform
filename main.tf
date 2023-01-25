af81107530be0c3900d0063d04d6e7af0d1106cd
Maxime LABELLE <maxime.labelle@chanel.com>
2023-01-25 08:24:48 +0100

    Initial commit.

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "azurerm" {
+    features                                              {}
+    alias                                                 = "az"
+    subscription_id                                       = var.subscription_id
+    tenant_id                                             = var.tenant_id
+}
+
+provider "azurerm" {
+    features                                              {}
+    alias                                                 = "az"
+    subscription_id                                       = var.subscription_id
+    tenant_id                                             = var.tenant_id
+}
